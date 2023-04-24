80a552edf875f764fd12052f6645dbbb3fc86ab4
stwang <464905943@qq.com>
2023-04-24 10:54:51 +0800

    update im ui

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
 import LemonIMUI from 'lemon-imui'
 import 'lemon-imui/dist/index.css'
 
 Vue.config.productionTip = false
 Vue.use(LemonIMUI)
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  render: (h) => h(App)
+}).$mount('#app')
