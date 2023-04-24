8a1d35918985108f2d8675e5fda6aac4950511a4
stwang <464905943@qq.com>
2023-04-24 09:30:57 +0800

    update ui

--- src/components/im.vue
+++ src/components/im.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="hello">
+  <div class="wrap">
     <lemon-imui ref="IMUI" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'IM',
 
   data () {
     return {
@@ -18,18 +18,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.wrap{
+ width: 100%;
+ height: 100vh;
+ display: flex;
+ align-items: center;
+ justify-content: center;
 }
 </style>
